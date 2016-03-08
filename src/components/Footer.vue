1a4d46f14db715a9339d8b402bd07ec919bfa807
jingsam <jingsam@163.com>
2016-03-09 00:21:16 +0800

    initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <footer class="mdl-mini-footer">
+    <div class="mdl-mini-footer__left-section">
+      <div class="mdl-logo">
+        FoxGIS
+      </div>
+      <ul class="mdl-mini-footer__link-list">
+        <li><a href="#">产品介绍</a></li>
+        <li><a href="#">企业合作</a></li>
+        <li><a href="#">关于我们</a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <div class="mdl-layout mdl-js-layout">
+    <header class="mdl-layout__header">
+      <img class="mdl-layout-icon" src="/dist/foxgis.png" alt="logo">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout__title">FoxGIS</span>
+        <div class="mdl-layout-spacer"></div>
+        <nav class="mdl-navigation">
+          <a class="mdl-navigation__link" href="#">图集</a>
+          <a class="mdl-navigation__link" href="#">数据</a>
+          <a class="mdl-navigation__link" href="#">制图</a>
+          <a class="mdl-navigation__link" href="#">社区</a>
+        </nav>
+      </div>
+    </header>
+    <div class="mdl-layout__drawer mdl-layout--small-screen-only">
+      <span class="mdl-layout__title">FoxGIS</span>
+        <nav class="mdl-navigation">
+          <a class="mdl-navigation__link" href="#">图集</a>
+          <a class="mdl-navigation__link" href="#">数据</a>
+          <a class="mdl-navigation__link" href="#">制图</a>
+          <a class="mdl-navigation__link" href="#">社区</a>
+        </nav>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+img {
+  max-width: 100%;
+}
+</style>
