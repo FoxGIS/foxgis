44f753a687837cdb7a0916263570c34c64c4b519
jingsam <jingsam@163.com>
2016-03-15 10:51:05 +0800

    add layout.vue

--- src/components/Atlas.vue
+++ src/components/Atlas.vue
@@ -1,4 +1,5 @@
 <template>
+<foxgis-layout>
 	<div id="atlas-container" class="mdl-grid">
 		<div v-for="item in atlas"  class="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--3-col"  v-bind:style="item.backgroundStyle"  >
 		  <div class="mdl-card__title">
@@ -20,58 +21,68 @@
 		  </div>
 		</div>
 	</div>
+</foxgis-layout>
 </template>
 
-<script type="text/javascript">
-  export default {
-  	data:() => {
-  		return {
-  			'atlas': [{
-  				name: '地图',
-  				introduction: '地图介绍',
-  				url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.eyJ1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
-  				backgroundStyle: {
-  					backgroundImage:"url('http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg')"
-  				}
-  			},{
-  				name: '地图',
-  				introduction: '地图介绍',
-  				url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.eyJ1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
-  				backgroundStyle: {
-  					backgroundImage:"url('http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg')"
-  				}
-  			},{
-  				name: '地图',
-  				introduction: '地图介绍',
-  				url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.eyJ1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
-  				backgroundStyle: {
-  					backgroundImage:"url('http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg')"
-  				}
-  			},{
-  				name: '地图',
-  				introduction: '地图介绍',
-  				url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.eyJ1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
-  				backgroundStyle: {
-  					backgroundImage:"url('http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg')"
-  				}
-  			},{
-  				name: '地图',
-  				introduction: '地图介绍',
-  				url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.eyJ1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
-  				backgroundStyle: {
-  					backgroundImage:"url('http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg')"
-  				}
-  			},{
-  				name: '地图',
-  				introduction: '地图介绍',
-  				url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.eyJ1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
-  				backgroundStyle: {
-  					backgroundImage:"url('http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg')"
-  				}
-  			}]
-  		}
-  	}
-  }
+<script>
+import Layout from './Layout'
+
+export default {
+  components: {
+    foxgisLayout: Layout
+  },
+  
+	data:() => {
+		return {
+			'atlas': [{
+				name: '地图',
+				introduction: '地图介绍',
+				url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+				backgroundStyle: {
+					backgroundImage:"url('http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg')"
+				}
+			},{
+				name: '地图',
+				introduction: '地图介绍',
+				url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+				backgroundStyle: {
+					backgroundImage:"url('http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg')"
+				}
+			},{
+				name: '地图',
+				introduction: '地图介绍',
+				url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+				backgroundStyle: {
+					backgroundImage:"url('http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg')"
+				}
+			},{
+				name: '地图',
+				introduction: '地图介绍',
+				url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+				backgroundStyle: {
+					backgroundImage:"url('http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg')"
+				}
+			},{
+				name: '地图',
+				introduction: '地图介绍',
+				url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+				backgroundStyle: {
+					backgroundImage:"url('http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg')"
+				}
+			},{
+				name: '地图',
+				introduction: '地图介绍',
+				url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+				backgroundStyle: {
+					backgroundImage:"url('http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg')"
+				}
+			}]
+		}
+	}
+}
 </script>
 
-<style type="text/css"></style>
+
+<style>
+
+</style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="mdl-layout mdl-js-layout">
+  <foxgis-header></foxgis-header>
+  <foxgis-drawer></foxgis-drawer>
+
+  <main class="mdl-layout__content">
+    <slot></slot>
+    <foxgis-footer></foxgis-footer>
+  </main>
+</div>
+</template>
+
+
+<script>
+import Header from './Header'
+import Drawer from './Drawer'
+import Footer from './Footer'
+
+export default {
+  components: {
+    foxgisHeader: Header,
+    foxgisDrawer: Drawer,
+    foxgisFooter: Footer
+  }
+}
+</script>
+
+
+<style scoped>
+.mdl-layout__content {
+  background-color: #f9f9f9;
+  overflow: visible;
+}
+
+</style>
