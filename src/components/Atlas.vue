9e804624bed361bf7971d3fdac20e0b00b85ac9e
ldllidonglin <ldllidonglin@sina.com>
2016-03-15 13:07:50 +0800

    fix atlas

--- src/components/Atlas.vue
+++ src/components/Atlas.vue
@@ -4,7 +4,8 @@
     <div id="search-tool" class="mdl-cell mdl-cell--12-col">
       <input /><i class="material-icons">search</i>
     </div>
-    <div v-for="item in atlas"  class="mdl-card mdl-shadow--4dp mdl-cell mdl-cell--3-col"  v-bind:style="item.backgroundStyle"  >
+    <div v-for="item in atlas"  class="map-item-container mdl-card mdl-shadow--4dp mdl-cell mdl-cell--4-col mdl-cell--6-col-phone"  >
+      <img src="{{ item.backgroundImage }}">
       <div class="mdl-card__title">
         <h2 class="mdl-card__title-text">{{ item.name }}{{ $index+1 }}</h2>
       </div>
@@ -36,8 +37,47 @@ export default {
         introduction: '地图介绍',
         url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
         backgroundImage:'http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg'
-        }
-      }]
+      },{
+        name: '地图',
+        introduction: '地图介绍',
+        url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+        backgroundImage:'http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg'
+      },{
+        name: '地图',
+        introduction: '地图介绍',
+        url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+        backgroundImage:'http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg'
+      },{
+        name: '地图',
+        introduction: '地图介绍',
+        url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+        backgroundImage:'http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg'
+      },{
+        name: '地图',
+        introduction: '地图介绍',
+        url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+        backgroundImage:'http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg'
+      },{
+        name: '地图',
+        introduction: '地图介绍',
+        url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+        backgroundImage:'http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg'
+      },{
+        name: '地图',
+        introduction: '地图介绍',
+        url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+        backgroundImage:'http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg'
+      },{
+        name: '地图',
+        introduction: '地图介绍',
+        url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+        backgroundImage:'http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg'
+      },{
+        name: '地图',
+        introduction: '地图介绍',
+        url: 'https://api.mapbox.com/styles/v1/donglinge/cilrqqik0007f9jm3qciil3dr.html?title=true&access_token=pk.J1IjoiZG9uZ2xpbmdlIiwiYSI6Ik1VbXI1TkkifQ.7ROsya7Q8kZ-ky9OmhKTvg#3.32/35.53/108.80',
+        backgroundImage:'http://www.weimeixi.com/uploads/allimg/141014/0S1045045-0.jpg'
+      },]
     }
   }
 }
@@ -46,11 +86,13 @@ export default {
 
 
 <style scoped>
-
+  #atlas-container {
+    padding: 10px 15px;
+  }
   #search-tool {
     margin: 10px 25% 10px 25%;
     border-radius: 20px;
-    border: solid 0px rgba(0, 0, 0, 0.74902);
+    border: solid 1px rgba(0, 0, 0, 0.14902);
     padding-left: 10px;
     background-color: #f9f9f9;
   }
@@ -68,6 +110,12 @@ export default {
     outline: none;
     font-size: 22px;
     background-color: #f9f9f9;
+  }
+  @media (min-width: 840px){
+    .mdl-cell--4-col {
+      margin: 25px;
+      width: calc(33.3333% - 50px);
+    }
   }
   #item-subtitle{
     display: flex;
