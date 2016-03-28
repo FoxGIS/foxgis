24b0f49cbed718ffb0ce918f6205f4126ffa4e3b
ldllidonglin <ldllidonglin@sina.com>
2016-03-28 09:22:05 +0800

    fix mapeditor

--- src/components/MapEditor.vue
+++ src/components/MapEditor.vue
@@ -2,14 +2,14 @@
   <div id="edit-wrap">
     <div id="toc">
       <div id="layer-control">
-        <div class="layer"><i class="material-icons">folder</i><span>图层1</span></div>
-        <div class="layer"><i class="material-icons">folder</i><span>图层2</span></div>
-        <div class="layer"><i class="material-icons">folder</i><span>图层3</span></div>
-        <div class="layer"><i class="material-icons">folder</i><span>图层4</span></div>
-        <div class="layer"><i class="material-icons">folder</i><span>图层5</span></div>
+        <div class="layer"><i class="material-icons">play_arrow</i><i class="material-icons">folder</i><span>图层1</span></div>
+        <div class="layer"><i class="material-icons">play_arrow</i><i class="material-icons">folder</i><span>图层2</span></div>
+        <div class="layer"><i class="material-icons">play_arrow</i><i class="material-icons">folder</i><span>图层3</span></div>
+        <div class="layer"><i class="material-icons">play_arrow</i><i class="material-icons">folder</i><span>图层4</span></div>
+        <div class="layer"><i class="material-icons">play_arrow</i><i class="material-icons">folder</i><span>图层5</span></div>
       </div>
       <div id="map-tool">
-        <mdl-anchor-button accent raised v-mdl-ripple-effect>保存</mdl-anchor-button>
+        <mdl-anchor-button accent raised v-mdl-ripple-effect>分享</mdl-anchor-button>
         <mdl-anchor-button accent raised v-mdl-ripple-effect>打印</mdl-anchor-button>
       </div>
 
@@ -35,22 +35,27 @@ body {
 
 #toc {
   flex:1;
-  padding: 20px;
+  padding: 0px 10px 0 10px;
+  margin-top: 10px;
+  height: 100%;
 }
 
 #layer-control {
-  padding-top: 20px;
+  padding-top: 5px;
+  border:solid 1px rgba(0,0,0,0.5);
+  background-color: rgba(237, 233, 217,0.4);
+  height: 84%;
 }
 
 .layer {
   vertical-align: middle;
-  font-size: 20px;
-  line-height: 20px;
+  font-size: 12px;
+  line-height: 12px;
   padding-bottom: 5px;
 }
 
 .layer i {
-  font-size: 20px;
+  font-size: 12px;
 }
 
 #map-tool {
