56a722f785e149920d5da72100ffeb8a9fa843fe
wanyanyan <wanyanyan91@163.com>
2016-06-12 11:29:29 +0800

    add user control

--- src/components/Studio.vue
+++ src/components/Studio.vue
@@ -5,10 +5,10 @@
     <div class="mdl-layout__drawer">
       <!-- <span class="mdl-layout-title">制图工作室</span> -->
       <nav class="mdl-navigation">
-        <a class="mdl-navigation__link" style="display: none;" v-link="{ path: '/studio/maps' }"><i class="material-icons">map</i>制图工程</a>
-        <a class="mdl-navigation__link" style="display: none;" v-link="{ path: '/studio/data' }"><i class="material-icons">layers</i>数据</a>
-        <a class="mdl-navigation__link" style="display: none;" v-link="{ path: '/studio/fonts' }"><i class="material-icons">text_format</i>字体</a>
-        <a class="mdl-navigation__link" style="display: none;" v-link="{ path: '/studio/icons' }"><i class="material-icons">place</i>符号</a>
+        <a class="mdl-navigation__link" v-if="access === true" v-link="{ path: '/studio/maps' }"><i class="material-icons">map</i>制图工程</a>
+        <a class="mdl-navigation__link" v-if="access === true" v-link="{ path: '/studio/data' }"><i class="material-icons">layers</i>数据</a>
+        <a class="mdl-navigation__link" v-if="access === true" v-link="{ path: '/studio/fonts' }"><i class="material-icons">text_format</i>字体</a>
+        <a class="mdl-navigation__link" v-if="access === true" v-link="{ path: '/studio/icons' }"><i class="material-icons">place</i>符号</a>
         <a class="mdl-navigation__link" v-link="{ path: '/studio/uploads' }"><i class="material-icons">image</i>决策用图<span  v-mdl-badge.number="upload_nums" ></span></a>
       </nav>
       <div class="mdl-layout-spacer"></div>
@@ -54,13 +54,17 @@ export default {
     }else{
       this.username = username
     }
-
+    this.$http({url:'/static/whitelist.json',method:'GET'}).then(function(response){
+      var accessUser = response.data.usernames;
+      if(accessUser.indexOf(this.username)!==-1){this.access=true;}
+    })
     console.log(this.$children[0].$children[0].uploads)
   },
   data: function(){
     return {
       username: '用户',
-      upload_nums:0
+      upload_nums:0,
+      access:false
     }
   },
   events: {
