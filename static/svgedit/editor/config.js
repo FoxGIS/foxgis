9a2049db00357a985d880f6dde510512b1f2b832
chenlisheng <335637257@qq.com>
2016-08-24 09:47:24 +0800

    edit bug

--- static/svgedit/editor/config.js
+++ static/svgedit/editor/config.js
@@ -50,7 +50,7 @@ svgEditor.setConfig({
 // EXTENSION CONFIG
 svgEditor.setConfig({
 	extensions: [
-		'ext-overview_window.js', 'ext-markers.js', 'ext-connector.js', 'ext-grid.js', 'ext-panning.js','ext-compass.js',
+		'ext-overview_window.js', 'ext-markers.js', 'ext-connector.js', 'ext-panning.js','ext-compass.js',
 			'ext-legend.js','ext-scale.js'
 	], 
 	noDefaultExtensions: true // noDefaultExtensions can only be meaningfully used in config.js or in the URL
