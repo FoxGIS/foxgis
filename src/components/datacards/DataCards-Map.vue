91e7f5cb9c9b9ffa9cd9b75e0ff92ad401401533
chenlisheng <335637257@qq.com>
2016-11-12 16:39:47 +0800

    修改样式

--- src/components/datacards/DataCards-Map.vue
+++ src/components/datacards/DataCards-Map.vue
@@ -18,9 +18,9 @@
         <td>
           {{dataset[(pageConfig.current_page-1)*pageConfig.page_item_num+$index].updatedAt}}
         </td>
-        <td style="color: #2f80bc;">
-          <a v-link="{ path: '/mapeditor/'+dataset[(pageConfig.current_page-1)*pageConfig.page_item_num+$index].style_id }">编辑</a>|
-          <span v-on:click="deleteStyle(dataset[(pageConfig.current_page-1)*pageConfig.page_item_num+$index].style_id)">删除</span>
+        <td>
+          <a class="edit" v-link="{ path: '/mapeditor/'+dataset[(pageConfig.current_page-1)*pageConfig.page_item_num+$index].style_id }"><span>编辑<span></a>|
+          <mdl-anchor-button colored v-mdl-ripple-effect v-on:click="deleteStyle(dataset[(pageConfig.current_page-1)*pageConfig.page_item_num+$index].style_id)">删除</mdl-anchor-button>
         </td>
       </tr>
     </table>
@@ -80,10 +80,12 @@ export default {
   border-collapse: collapse;
   table-layout: fixed;
 }
+
 .wrapper table th {
   padding: 10px 0px;
   background-color: #eee;
 }
+
 .wrapper table td {
   padding: 10px 0px;
   background-color: #ffffff;
@@ -92,17 +94,41 @@ export default {
   overflow:hidden;/* 内容超出宽度时隐藏超出部分的内容 */
   text-overflow:ellipsis;/* 当对象内文本溢出时显示省略标记(...) ；需与overflow:hidden;一起使用。*/
 }
-.wrapper table td a {
-  text-decoration: none;
-  color: #2f80bc;
-  padding-right: 5px;
-}
-.wrapper table td span {
-  color: #2f80bc;
-  cursor: pointer;
-  padding-left: 5px;
-}
+
 .wrapper table tr:nth-child(odd) td {
   background-color: #eee;
+}
+
+.wrapper table td a.edit {
+  text-decoration: none;
+  color: #2f80bc;
+  display: inline-block;
+  font-size: 14px;
+  font-weight: 500;
+  width: 40px;
+  line-height: 24px;
+  height: 24px;
+  min-width: inherit;
+}
+
+.wrapper table td a.edit:hover {
+  border: none;
+  border-radius: 2px;
+  background-color:#ececec;
+}
+
+.wrapper table td a.edit span {
+  display: inline-block;
+  position: relative;
+  top: 2px;
+}
+
+.wrapper .mdl-button {
+  padding: 0;
+  width: 40px;
+  line-height: 24px;
+  height: 24px;
+  min-width: inherit;
+  color: #2f80bc;
 }
 </style>
